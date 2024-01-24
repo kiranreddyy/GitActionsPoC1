b20b38cb73ef95dbc51996281cc528d4f86b0963
kiranreddyy <kiranreddyy@gmail.com>
2024-01-24 11:37:58 +0530

    Update init.ps1

--- powershell/init.ps1
+++ powershell/init.ps1
@@ -1,7 +1,7 @@
 
 $image_def = "raji"
 #common Messages Json file path
-$jsonMessagesPath ='Common/Messages.json'
+$jsonMessagesPath ='powershell/Messages.json'
 $jMessages = Get-Content -Path $jsonMessagesPath -Raw | ConvertFrom-Json
 Write-Host "$($jMessages.e_gallery_c_id_v_c -f $image_def)"
  
