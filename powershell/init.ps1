fbc9ebaeef5ec2214c1c9f1ce60aa0fa4c921d2f
kiranreddyy <kiranreddyy@gmail.com>
2024-01-29 19:25:44 +0530

    Update init.ps1

--- powershell/init.ps1
+++ powershell/init.ps1
@@ -5,3 +5,10 @@ $jsonMessagesPath ='powershell/Messages.json'
 $jMessages = Get-Content -Path $jsonMessagesPath -Raw | ConvertFrom-Json
 Write-Host "$($jMessages.e_gallery_c_id_v_c -f $image_def)"
  
+#echo 'my_param=my_value' | tee $GITHUB_OUTPUT
+#echo 'my_env_var=my_value' | tee $GITHUB_ENV
+
+$myv1_val = 'kiran'
+$myv2_val = 'reddy'
+echo "myv1=$myv1_val" >>$env:GITHUB_OUTPUT
+echo "myv2=$myv2_val" >>$env:GITHUB_ENV
